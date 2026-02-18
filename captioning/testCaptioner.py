0435d5a1a3ea4038866eddf00755950b82cf64c2
Shokoofeh <hoseini.shokoofeh@gmail.com>
2026-02-18 10:47:03 +0100

    remove files

--- captioning/testCaptioner.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from intervlCaptioner_OLD import InterVLCaptioner
-
-cap = InterVLCaptioner(
-    model_path="C:/SCIoI/weights/InternVL-Chat-2B-V1_5.pth"
-)
-
-dummy = np.zeros((200,200,3), dtype=np.uint8)
-print(cap.caption_object(dummy))
